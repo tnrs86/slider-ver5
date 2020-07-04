297b96457ec9ee6220f73516e3b77f55fa2e68aa
tnrs86 <tnrs86@yandex.ru>
2020-07-04 23:28:42 +0300

    Model module: add file

---
+++ src/scripts/model.ts
@@ -0,0 +1,26 @@
+export class Model {
+  currentValues: number[];
+  step: number;
+
+  setCurrentValues() {
+
+  }
+
+  getCurrenValues() {
+
+  }
+
+  possibleValueanalysis(possibleValue: number[]) {
+
+  }
+
+  calcCurrentValues() {
+    
+  }
+
+  checkPossibleMove():boolean {
+    let result: boolean;
+
+    return result;
+  }
+}
