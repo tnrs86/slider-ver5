b84a974599f974f945710f27016d1d73143f4a32
tnrs86 <tnrs86@yandex.ru>
2020-07-17 15:17:34 +0300

    Presenter module: added methods, edited methods
    Added: controlDataHandler (to control panel), externalDataHandler (to external recievers)
    Edited: init methods (working with control panel)

--- src/scripts/presenter.ts
+++ src/scripts/presenter.ts
@@ -7,8 +7,9 @@ export class Presenter {
   minPossibleValue: number;
   maxPossibleValue: number;
   rangePossibleValues: number;
-  startSelectedRange: number;
-  endSelectedRange: number;
+  //startSelectedRange: number;
+  //endSelectedRange: number;
+  currentRange: number[] = [];
   noNumericValues: boolean;
   noNumericScale: {};
   view: View;
@@ -22,52 +23,54 @@ export class Presenter {
     this.rangePossibleValues = this.maxPossibleValue - this.minPossibleValue;
 
     if (typeof options.startSelectedRange === 'undefined') {
-      this.startSelectedRange = this.minPossibleValue * 1.1;
+      //this.startSelectedRange = this.minPossibleValue * 1.1;
+      this.currentRange.push(this.minPossibleValue * 1.1);
     } else {
-      this.startSelectedRange = options.startSelectedRange
+      //this.startSelectedRange = options.startSelectedRange;
+      this.currentRange.push(options.startSelectedRange);
     }
 
     if (typeof options.endSelectedRange === 'undefined') {
-      this.endSelectedRange = this.maxPossibleValue * 0.9;
+      //this.endSelectedRange = this.maxPossibleValue * 0.9;
+      this.currentRange.push(this.maxPossibleValue * 0.9);
     } else {
-      this.endSelectedRange = options.endSelectedRange;
+      //this.endSelectedRange = options.endSelectedRange;
+      this.currentRange.push(options.endSelectedRange);
     }    
     this.noNumericValues = options.noNumericValues
     this.view = view;
     this.model = model;
-
-    //this.init(rootElement, options.rangeMode, options.usefeedBack, options.useScale, options.verticalView);
   }
 
-  init(rootHTMLElement: HTMLElement, rangeMode: boolean, useFeedback: boolean, useScale: boolean, 
-      verticalView: boolean, step?: number, sliderTemplate?: {}): void {
+  init(options: SliderOptions, rootHTMLElement: HTMLElement): void {
     this.view.setBaseConfiguration(rootHTMLElement);
     
     let relativeCurrentValue: number[] = [];    
-    if (rangeMode) {
+    if (options.rangeMode) {
       this.view.setRangeMode();
-      relativeCurrentValue = this.getRelativeData( [this.startSelectedRange, this.endSelectedRange] ) as number[];     
+      //relativeCurrentValue = this.getRelativeData( [this.startSelectedRange, this.endSelectedRange] ) as number[];     
     } else {      
-      relativeCurrentValue = this.getRelativeData( [this.startSelectedRange] ) as number[];            
+     //relativeCurrentValue = this.getRelativeData( [this.startSelectedRange] ) as number[];            
     }
+    relativeCurrentValue = this.getRelativeData(this.currentRange) as number[];
     
-    if (useFeedback) {      
+    if (options.usefeedBack) {      
       this.view.setThumbFeedbacks();
     }
 
-    this.view.setSliderOrientation(verticalView); 
+    this.view.setSliderOrientation(options.verticalView); 
     
-    if (useScale) {      
-      let scaleData: {} = this.getScaleData(sliderTemplate);
+    if (options.useScale) {      
+      let scaleData: {} = this.getScaleData(options.noNumericScale);
       
-      if (this.noNumericValues && sliderTemplate) {
+      if (this.noNumericValues && options.noNumericScale) {
         this.noNumericScale = scaleData;
       } else {
         this.noNumericValues = false;
       }
 
       this.view.setScale(scaleData);
-      this.view.setSliderOrientation(verticalView, this.view.sliderScale);
+      this.view.setSliderOrientation(options.verticalView, this.view.sliderScale);
     }   
     
     this.view.setListeners(this.positionHandler.bind(this), this.resizeHandler.bind(this));
@@ -75,17 +78,31 @@ export class Presenter {
     this.view.setElementPositions(relativeCurrentValue); 
     
     this.model.setCurrentValues(relativeCurrentValue);
-    if (step) {
-      this.model.setStep(step / this.rangePossibleValues);
-    }        
+    if (options.step) {
+      this.model.setStep(options.step / this.rangePossibleValues);
+    }
+    
+    this.view.setControlPanel(rootHTMLElement, options.externalRecievers[0], options.externalRecievers[1]);
+    this.view.setControlPanelParameters({'min-value': this.minPossibleValue, 
+                                        'max-value': this.maxPossibleValue, 
+                                        'step': options.step, 
+                                        'scale': options.useScale, 
+                                        'vertical': options.verticalView, 
+                                        'feedback': options.usefeedBack,
+                                        'range-mode': options.rangeMode});
+
+    this.view.setControlPanelListener(this.controlDataHandler.bind(this));
   }
 
   positionHandler(position: number, index?: number) {
     let currentPosition = this.model.possibleValueAnalysis(position, index);
 
     if (currentPosition != -1) {
-      let feedbackContent: string = ''+ this.getAbsoluteData(currentPosition);
+      let absoluteData: number = this.getAbsoluteData(currentPosition) as number;
+      
+      this.currentRange[index] = absoluteData;
 
+      let feedbackContent: string = ''+ absoluteData;
       this.view.setElementPositions(currentPosition, index, feedbackContent);
     }
   }
@@ -95,6 +112,106 @@ export class Presenter {
       this.view.removeScale();
       this.view.setScale(this.getScaleData());
       this.view.setSliderOrientation(undefined, this.view.sliderScale);
+    }
+  }
+
+  externalDataHandler(value: string, index: number) {
+    let position: number = this.getRelativeData(parseInt(value)) as number;
+    let currentPosition = this.model.possibleValueAnalysis(position, index);
+
+    if (currentPosition != -1) {
+      let feedbackContent: string = ''+ this.getAbsoluteData(currentPosition);
+
+      this.view.setElementPositions(currentPosition, index, feedbackContent);
+    }  
+  }
+
+  controlDataHandler(value: number| boolean, hint: string) {
+    let relativeCurrentValue: number[];
+    switch (true) {
+      case hint == 'min-value':
+        this.minPossibleValue = value as number;
+        this.rangePossibleValues = this.maxPossibleValue - this.minPossibleValue;
+        relativeCurrentValue = this.getRelativeData(this.currentRange) as number[];
+        this.view.setElementPositions(relativeCurrentValue);
+        
+        if (this.view.sliderScale) {          
+          this.view.removeScale();
+          this.view.setScale(this.getScaleData());
+          this.view.setSliderOrientation(this.view.verticalView, this.view.sliderScale);
+        }
+        break;
+      case hint == 'max-value':
+        this.maxPossibleValue = value as number;
+        this.rangePossibleValues = this.maxPossibleValue - this.minPossibleValue;
+        relativeCurrentValue = this.getRelativeData(this.currentRange) as number[];
+        this.view.setElementPositions(relativeCurrentValue);
+        
+        if (this.view.sliderScale) {          
+          this.view.removeScale();
+          this.view.setScale(this.getScaleData());
+          this.view.setSliderOrientation(this.view.verticalView, this.view.sliderScale);
+        }
+        break;
+      case hint == 'step':
+        this.model.setStep(value as number / this.rangePossibleValues);
+        break;
+      case hint == 'scale':
+        console.log(value)
+        if (value) {
+          if (!this.view.sliderScale) {
+            let scaleData: {} = this.getScaleData(this.noNumericScale);
+            this.view.setScale(scaleData);
+            this.view.setSliderOrientation(this.view.verticalView, this.view.sliderScale);
+          }
+        } else {
+          if (this.view.sliderScale) {
+            this.view.removeScale()
+          }
+        }
+        break;
+      case hint == 'vertical':
+        if (value !== this.view.verticalView) {
+          this.view.setSliderOrientation(value as boolean);
+          relativeCurrentValue = this.getRelativeData(this.currentRange) as number[];
+          this.view.setElementPositions(relativeCurrentValue);
+          
+          if (this.view.sliderScale && this.checkChangeScale()) {
+            this.view.removeScale();
+            this.view.setScale(this.getScaleData());
+            this.view.setSliderOrientation(undefined, this.view.sliderScale);
+          }
+        }
+        break;
+      case hint == 'feedback':
+        
+        if (value !== this.view.useFeedback) {          
+          if (value) {
+            console.log('fb')
+            this.view.setThumbFeedbacks();
+          } else {
+            this.view.removeThumbFeedback();
+          }
+        }
+        break;
+      
+      case hint == 'range-mode':
+        if (value !== this.view.rangeMode) {
+          if (value) {
+            this.view.setRangeMode();
+            this.view.setSliderOrientation(undefined, this.view.sliderThumbs[1]);
+            this.view.setSliderOrientation(undefined, this.view.sliderFiller);
+            this.model.currentValues.push(this.model.currentValues[0] * 1.5);
+            this.view.setElementPositions(this.model.currentValues);
+            if (this.view.setThumbFeedbacks) {
+              this.view.setThumbFeedbacks();
+            }
+          } else {
+            this.view.setSingleMode();
+            this.model.currentValues.length = 1;
+          }
+        }
+      default:       
     }
   }
 
@@ -110,6 +227,7 @@ export class Presenter {
       }
       
     } else {
+
       let sumProperties: number = 0;
      
       for (let key in sliderTemplate) {
