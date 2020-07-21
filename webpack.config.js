bfbf5175aa50ca79c8dfc19e00e6ffd3b58f0db6
tnrs86 <tnrs86@yandex.ru>
2020-07-22 00:21:31 +0300

    webpack.config: add file

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = path.join(__dirname, '../src');
+  
+const appConfig = {
+  mode: 'development',
+  entry: './src/apps/view.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader
+         ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  }, 
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/page/page.pug',
+      filename: './about/page-1.html',
+      inject: true
+    })
+  ],
+};
+
+const testConfigMocha = {
+  mode: 'development',
+  entry: './tests/view/view_test2.ts', // <=== can be omitted as default is 'web'
+  output: {
+    path: path.resolve(__dirname, 'test'),
+    filename: 'view.test.js'    
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.js$|\.jsx$/,
+        use: {
+          loader: 'istanbul-instrumenter-loader',
+          options: { esModules: true }
+        },
+        enforce: 'post',
+        exclude: /node_modules|\.spec\.js$/,
+      }
+    ]
+  },
+  node: {
+    module: "empty",
+    fs: "empty",
+    child_process: "empty"
+  },
+  externals: {
+    puppeteer: 'require("puppeteer")',
+    jsdom: 'require("jsdom")',
+    jest: 'require("jest")',
+    expect: 'require("jest")'
+  },
+
+};
+
+module.exports = [ testConfigMocha ];
